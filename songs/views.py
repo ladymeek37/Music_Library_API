bd40cc9421168535f5502042d2e4f68aef57ced2
Lady Meek <ladyjmeek@gmail.com>
2023-02-07 14:25:01 -0800

    created data and get all songs request in postman

--- songs/views.py
+++ songs/views.py
@@ -1,10 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
+
 
 
 @api_view(['GET'])
 def songs_list(request):
 
+    songs = Song.objects.all()
 
-    return Response('ok')
+    serializer = SongSerializer(songs, many=True)
+
+    return Response(serializer.data)
 
